79e629393a49bf8c31dc72cf95306106d2db99a4
Horpe <oniopeyemifrancis@gmail.com>
2024-08-23 18:10:58 +0100

    unrelated relationship LWC

---
+++ force-app/main/default/lwc/counts/counts.js
@@ -0,0 +1,27 @@
+import { LightningElement, wire } from 'lwc';
+import { subscribe, MessageContext } from 'lightning/messageService';
+import COUNT_UPDATED_CHANNEL from '@salesforce/messageChannel/Count_Updated__c';
+
+export default class Counts extends LightningElement {
+    subscription = null;
+    priorCount = 0;
+    counter = 0;
+    @wire(MessageContext)
+    MessageContext;
+    subscribeToMessageChannel(){
+        this.subscription = subscribe(this.MessageContext, COUNT_UPDATED_CHANNEL, (message) => this.handleMessage(message));
+    }
+    handleMessage(message){
+        this.priorCount = this.counter;
+        if(message.operator == 'add'){
+            this.counter += message.constant;
+        }else if(message.operator == 'subtract'){
+            this.counter -= message.constant;
+        }else{
+            this.counter *= message.constant;
+        }
+    }
+    connectedCallback(){
+        this.subscribeToMessageChannel();
+    }
+}
