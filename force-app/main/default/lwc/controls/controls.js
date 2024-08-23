7998b4b3bd57ee8f37c86107b43634bebf23f568
Horpe <oniopeyemifrancis@gmail.com>
2024-08-23 14:01:43 +0100

    added a multiplier controller

---
+++ force-app/main/default/lwc/controls/controls.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+
+export default class Controls extends LightningElement {
+    handleAdd(){
+        this.dispatchEvent(new CustomEvent('add'));
+    }
+    handleSubtract(){
+        this.dispatchEvent(new CustomEvent('subtract'));
+    }
+    handleMultiply(event){
+        const factor = event.target.dataset.factor;
+        this.dispatchEvent(new CustomEvent('Multiply', {detail: factor}));
+    }
+   
+}
